963869a9e6555883966c53df812dbfb723922bbf
RonaldRomisch <131470547+RonaldRomisch@users.noreply.github.com>
2023-10-07 16:16:59 +0200

    video 8 der ersten reihe

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,9 @@
 class Character extends MovableObject{
     
+    constructor() {
+        super().loadImage('C:/coding/Developer Akademie/Modul 11/ElPolloLoco/img/2_character_pepe/2_walk/W-21.png');
+    }
+
     jump() {
 
     }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,3 +1,6 @@
 class Chicken extends MovableObject {
     
+    constructor() {
+        super().loadImage('C:/coding/Developer Akademie/Modul 11/ElPolloLoco/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+    }
 }
