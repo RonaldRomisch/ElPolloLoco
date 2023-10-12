c54e387525e1d6012b89565719a89854c543f6cd
RonaldRomisch <131470547+RonaldRomisch@users.noreply.github.com>
2023-10-12 21:43:53 +0200

    video 15 season 2

--- models/world.class.js
+++ models/world.class.js
@@ -8,6 +8,9 @@ class World {
     clouds = [
         new Cloud()
     ];
+    backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 80)
+    ];
     canvas;
     ctx;
 
@@ -20,19 +23,27 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        });
+        this.addObjectsToMap(this.backgroundObjects);
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
 
-        this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-        });
+
 
         //draw() wird immer wieder aufgerufen
         let self = this;
         requestAnimationFrame(function() {
-            self.draw();//the word this is not known in the function requestAnimationFrame
+            self.draw();//the word "this" is not known in the function requestAnimationFrame
         });
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
+
+    addToMap(mo) {
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height)
     }
 }
