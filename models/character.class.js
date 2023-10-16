33775d83de90495d31c66a70b0346cc63e4e99ca
RonaldRomisch <131470547+RonaldRomisch@users.noreply.github.com>
2023-10-16 19:11:51 +0200

    first season finished

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,10 @@
 class Character extends MovableObject{
     
+    y = 90;
+
+    height = 300;
+    width = 150;
+
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,9 +1,10 @@
 class Chicken extends MovableObject {
-    y = 240;
+    y = 280;
+    height = 100;
     
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
-        this.x = 200 + Math.random() * 500;
+        this.x = 280 + Math.random() * 500;
     }
 }
