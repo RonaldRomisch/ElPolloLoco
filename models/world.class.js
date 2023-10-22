c9cbe861eb3a892d4f36e42312c1097538013f80
RonaldRomisch <131470547+RonaldRomisch@users.noreply.github.com>
2023-10-22 19:18:09 +0200

    season 3 video 12

--- models/world.class.js
+++ models/world.class.js
@@ -9,14 +9,27 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/air.png', -719, 400),
+        new BackgroundObject('img/5_background/layers/3_third_layer/full.png', -719, 400),
+        new BackgroundObject('img/5_background/layers/2_second_layer/full.png', -719, 140),
+        new BackgroundObject('img/5_background/layers/1_first_layer/full.png', -719, 120),
         new BackgroundObject('img/5_background/layers/air.png', 0, 400),
         new BackgroundObject('img/5_background/layers/3_third_layer/full.png', 0, 400),
         new BackgroundObject('img/5_background/layers/2_second_layer/full.png', 0, 140),
-        new BackgroundObject('img/5_background/layers/1_first_layer/full.png', 0, 120)
+        new BackgroundObject('img/5_background/layers/1_first_layer/full.png', 0, 120),
+        new BackgroundObject('img/5_background/layers/air.png', 719, 400),
+        new BackgroundObject('img/5_background/layers/3_third_layer/full.png', 719, 400),
+        new BackgroundObject('img/5_background/layers/2_second_layer/full.png', 719, 140),
+        new BackgroundObject('img/5_background/layers/1_first_layer/full.png', 719, 120),
+        new BackgroundObject('img/5_background/layers/air.png', 719 * 2, 400),
+        new BackgroundObject('img/5_background/layers/3_third_layer/full.png', 719 * 2, 400),
+        new BackgroundObject('img/5_background/layers/2_second_layer/full.png', 719 * 2, 140),
+        new BackgroundObject('img/5_background/layers/1_first_layer/full.png', 719 * 2, 120)
     ];
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
 
     constructor(canvas, keyboard) {
@@ -34,12 +47,14 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
+        this.ctx.translate(this.camera_x, 0); // Verschiebung der Kamera nach links
+
         this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
 
-
+        this.ctx.translate(-this.camera_x, 0);
 
         //draw() wird immer wieder aufgerufen
         let self = this;
@@ -55,6 +70,16 @@ class World {
     }
 
     addToMap(mo) {
+        if (mo.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(mo.width, 0);
+            this.ctx.scale(-1, 1);
+            mo.x = mo.x * -1;
+        }
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height)
+        if (mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
     }
 }
