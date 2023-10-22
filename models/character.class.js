6173103db34832d75cc9be145c17997bfdbcdf29
RonaldRomisch <131470547+RonaldRomisch@users.noreply.github.com>
2023-10-22 13:54:04 +0200

    season 3 video 7

--- models/character.class.js
+++ models/character.class.js
@@ -15,6 +15,7 @@ class Character extends MovableObject{
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png'
     ];
+    world;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
@@ -26,10 +27,12 @@ class Character extends MovableObject{
     animate() {
 
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length;
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
+            if (this.world.keyboard.RIGHT) {
+                let i = this.currentImage % this.IMAGES_WALKING.length;
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
         }, 100);
         
     }
