d9eb2f8d2a1cb7b4347246518806e8d1b8d164cf
RonaldRomisch <131470547+RonaldRomisch@users.noreply.github.com>
2023-10-28 11:17:24 +0200

    opitimze jumping

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -13,7 +13,7 @@ class MovableObject {
     
     applyGravity() {
         setInterval(() => {
-            if(this.isAboveGround()) {
+            if(this.isAboveGround() || this.speedY > 0) {
                 this.y -= this.speedY;
                 this.speedY -= this.acceleration;
             }
@@ -51,12 +51,10 @@ class MovableObject {
     }
 
     moveRight () {
-        console.log('Moving right');
+        this.x += this.speed;
     }
 
     moveLeft() {
-        setInterval( () => {
-            this.x -= this.speed;
-        }, 1000 / 60);
+        this.x -= this.speed;
     }
 }
